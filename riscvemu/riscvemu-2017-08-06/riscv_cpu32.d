061db39df1e696458e3164e9e0dc675961158b48
yamaguchi1024 <yukatkh@yahoo.co.jp>
2018-01-23 13:38:35 +0900

    riscvemu 色々消した

--- riscvemu/riscvemu-2017-08-06/riscv_cpu32.d
+++
@@ -1,2 +0,0 @@
-riscv_cpu32.o: riscv_cpu.c cutils.h iomem.h riscv_cpu.h softfp.h \
- riscvemu_template.h riscvemu_fp_template.h
--- riscvemu/riscvemu-2017-08-06/riscvemu.d
+++
@@ -1,2 +0,0 @@
-riscvemu.o: riscvemu.c cutils.h iomem.h virtio.h pci.h fs.h machine.h \
- json.h fs_utils.h fs_wget.h slirp/libslirp.h
